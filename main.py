39202167c51c1c7a6dbe5f8b6e4b1c082b291b72
Abhronil Dutta <177771087+Abhronil-Dutta@users.noreply.github.com>
2025-12-29 23:51:09 -0700

    Added get message tool

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Auth.main import auth
-from tools.gmail_tools import search_messages
+from tools.gmail_tools import search_messages, get_message
 
 if __name__ == "__main__":
     service = auth("Test")
-    print(search_messages(service, query="Quora" ,max_results = 1))
+    msg = search_messages(service, query="Test" ,max_results = 1)
+    print(get_message(service=service, message_id=msg[0].get('id')))
--- tools/gmail_tools.py
+++ tools/gmail_tools.py
@@ -29,6 +29,48 @@ def search_messages(service, query: str, max_results: int = 10) -> list:
         resp = service.users().messages().list(userId='me', q= query, maxResults= max_results).execute()
         return resp.get('messages', [])
     except HttpError as e:
-        print(f"Got error {e}")
-        return []
+        raise RuntimeError(f"Got search_message error: {e}") from e
+    
+# --- Tools : Get Message ---
 
+def get_message(service, message_id: str, format : str = "full"):
+
+    try: 
+        msg = service.users().messages().get(userId='me', id=message_id, format = format).execute()
+    except HttpError as e:
+        raise RuntimeError(f"Got get_message error: {e}") from e
+
+    #Extract the Headers
+    headers = {
+        h['name']: h['value']
+        for h in msg.get('payload', {}).get('headers', [])
+    }
+
+    # Body
+    body_text = None
+    if format in ("full","raw"):
+
+        # parts because emails are MIME trees
+        parts = msg.get('payload', {}).get('parts') or []
+
+        for p in parts:
+            # MIME Types 'text/plain' , 'text/html', 'multipart/*', 'application/pdf'
+            mime = p.get('mimeType', '')
+            #we wanna work with text/plain 
+            if mime == 'text/plain' and 'body' in p and 'data' in p['body']:
+
+                #Decoding
+                body_text = base64.urlsafe_b64decode(p['body']['data'] + '==').decode('utf-8', errors='replace')
+                break
+
+            if not body_text:
+                # Snippet = Gmail's safe preview.. always present
+                body_text = msg.get('snippet')
+        
+        return {
+            "id" : msg.get('id'),
+            "threadId": msg.get('threadId'),
+            "headers": headers,
+            "body_text": body_text,
+            "raw": msg.get('raw')  # may be present if requested
+        }
